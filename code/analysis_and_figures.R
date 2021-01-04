502dab2228778900eecfbc883e07447909a295b5
rbolam <friederike.bolam@ncl.ac.uk>
2021-01-04 12:39:43 +0000

    Add analysis script

---
+++ code/analysis_and_figures.R
@@ -0,0 +1,10 @@
+## ---------------------- Data analysis and figures ----------------------------####
+
+
+library(tidyverse)
+library(googlesheets4)
+
+
+## Read in data:
+mepwell <- read_sheet("https://docs.google.com/spreadsheets/d/18TyJ9tJnrrwx9aEAyB2bmtnfWnFVXgjlRTCmZ1qoA-w/edit?usp=sharing")
+
