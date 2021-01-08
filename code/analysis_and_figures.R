5dcec8d9e8d2ccb58cbd1bd58bb845a7048a0a4d
lbraunholtz <l.d.braunholtz2@ncl.ac.uk>
2021-01-08 13:38:10 +0000

    adding some ideas for figures to create - but not the code yet!

--- code/analysis_and_figures.R
+++ code/analysis_and_figures.R
@@ -8,3 +8,5 @@ library(googlesheets4)
 ## Read in data:
 mepwell <- read_sheet("https://docs.google.com/spreadsheets/d/18TyJ9tJnrrwx9aEAyB2bmtnfWnFVXgjlRTCmZ1qoA-w/edit?usp=sharing")
 
+## Figure ideas - could just do a couple so that there are still some things to plot as a group in thursday meetings!
+## Most active team, most popular activity (lollipop plot?), peak/slump activity days (get mean/median and do deviation from plot), tracking folks activity levels throughout the week
